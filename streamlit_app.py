5da73476cc7ada0d1e7a412413e2a5737d68e1cd
Vladyslav Handzii <root.user76@gmail.com>
2023-11-10 00:55:57 +0100

    del streamlit gpt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,4 @@
 import streamlit as st
-from gpt4all import GPT4All
-model = GPT4All("mistral-7b-openorca.Q4_0.gguf")
-
-
 # Установка заголовка страницы
 st.title("Пример веб-приложения с Streamlit")
 
@@ -13,6 +9,6 @@ if st.button("Нажми меня"):
     # Добавление поля для ввода текста
 user_input = st.text_input("Введите текст", max_chars=80)
 
-with model.chat_session():
-    response1 = model.generate(prompt=user_input, temp=0)
-    st.write("Відповідь:", model.current_chat_session)
+    # Вывод текста, введенного пользователем
+st.write("Вы ввели:", user_input)
+
