1f2d1d56b840d700302b661a4b9eef76f74d8e96
Vladyslav Handzii <root.user76@gmail.com>
2023-11-10 14:14:11 +0100

    add streamlit spacy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,38 @@
 import streamlit as st
-# Установка заголовка страницы
-st.title("Пример веб-приложения с Streamlit")
+import spacy
+from spacy import displacy
 
-# Добавление кнопки
-if st.button("Нажми меня"):
-    st.write("Кнопка была нажата!")
+# Загрузка модели spaCy для английского языка
+nlp = spacy.load("en_core_web_sm")
 
-    # Добавление поля для ввода текста
-user_input = st.text_input("Введите текст", max_chars=80)
 
-    # Вывод текста, введенного пользователем
-st.write("Вы ввели:", user_input)
+# Функция для выделения глаголов в тексте
+def highlight_verbs(text):
+    doc = nlp(text)
+    highlighted_text = []
+    for token in doc:
+        if token.pos_ == "VERB" or (token.dep_ == "aux" and token.head.pos_ == "VERB"):
+            highlighted_text.append((token.text, "verb"))
+        else:
+            highlighted_text.append(token.text)
+    return highlighted_text
+
+
+# Веб-приложение с использованием Streamlit
+def main():
+    st.title("Text Highlighter")
+
+    # Поле для ввода текста пользователем
+    user_text = st.text_area("Введите текст:")
+
+    if st.button("Выделить глаголы"):
+        # Обработка текста и выделение глаголов
+        highlighted_text = highlight_verbs(user_text)
+
+        # Отображение выделенного текста
+        annotated_text(*highlighted_text)
+
+
+if __name__ == "__main__":
+    main()
 
