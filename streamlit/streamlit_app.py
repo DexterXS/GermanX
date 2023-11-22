e8dab73d998d8bd5c3a37ed11509d8c408be8d56
Vladyslav Handzii <root.user76@gmail.com>
2023-11-22 22:08:08 +0100

    add test

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -7,7 +7,6 @@ class WeatherAPI:
         self.base_url = "http://api.openweathermap.org/data/2.5/weather"
 
     def get_weather(self, city):
-        global response
         params = {"q": city, "appid": self.api_key, "units": "metric"}
         try:
             response = requests.get(self.base_url, params=params)
