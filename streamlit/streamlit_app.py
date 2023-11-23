1779a3762692f973ce46eb7b6a44355643c1e360
Vladyslav Handzii <root.user76@gmail.com>
2023-11-23 21:12:56 +0100

    add test streamlit

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,39 +1,81 @@
 import streamlit as st
-import requests
+import random
 
-class WeatherAPI:
-    def __init__(self, api_key):
-        self.api_key = api_key
-        self.base_url = "http://api.openweathermap.org/data/2.5/weather"
 
-    def get_weather(self, city):
-        params = {"q": city, "appid": self.api_key, "units": "metric"}
-        try:
-            response = requests.get(self.base_url, params=params)
-            response.raise_for_status()
+class Card:
+    def __init__(self, term, translation, category):
+        self.term = term
+        self.translation = translation
+        self.category = category
 
-            data = response.json()
-            return data
-        except requests.exceptions.HTTPError as errh:
-            st.exception(f"HTTP Error: {errh}")
-            return None
-        except requests.exceptions.RequestException as err:
-            st.exception(f"Request Exception: {err}")
-            return None
-        except requests.exceptions.JSONDecodeError as err:
-            st.exception(f"JSON Decode Error: {err}")
-            st.exception(f"Response Text: {response.text}")
-            return None
 
-st.title("Погодний додаток")
+class FlashcardApp:
+    def __init__(self):
+        if 'flashcards' not in st.session_state:
+            st.session_state.flashcards = []
 
-city = st.text_input("Введіть місто:", "Kyiv")
+    def add_flashcard(self):
+        term = st.text_input("Термін:")
+        translation = st.text_input("Переклад:")
+        category = st.text_input("Категорія:")
 
-api_key = "c6c8f1d7c5a3cb77f9213311afb839d3"
+        if term and translation and category:
+            st.session_state.flashcards.append(Card(term, translation, category))
+            st.success("Картку додано!")
 
-weather_api = WeatherAPI(api_key=api_key)
+    def study_flashcards(self):
+        if not st.session_state.flashcards:
+            st.warning("Додайте картку перед вивченням.")
+            return
 
-weather_data = weather_api.get_weather(city)
-if weather_data:
-    temperature = weather_data["main"]["temp"]
-    st.write(f"Погода у {city}: {temperature}°C")
+        random_card = random.choice(st.session_state.flashcards)
+        user_translation = st.text_input(f"Як перекладеться '{random_card.term}'?")
+
+        if user_translation:
+            if user_translation.lower() == random_card.translation.lower():
+                st.success("Вірно!")
+            else:
+                st.error(f"Правильний переклад: {random_card.translation}")
+
+    def view_all_flashcards(self):
+        if not st.session_state.flashcards:
+            st.warning("Додайте картку перед переглядом.")
+            return
+
+        st.header("Всі картки")
+        for card in st.session_state.flashcards:
+            st.button(f"{card.term}\nПереклад: {card.translation}\nКатегорія: {card.category}")
+
+    def choose_category(self):
+        categories = list(set(card.category for card in st.session_state.flashcards))
+        selected_category = st.selectbox("Виберіть категорію:", ["Всі"] + categories)
+
+        selected_flashcards = st.session_state.flashcards if selected_category == "Всі" else [
+            card for card in st.session_state.flashcards if card.category == selected_category
+        ]
+
+        st.header(f"Картки в категорії: {selected_category}")
+        for card in selected_flashcards:
+            st.button(f"{card.term}\nПереклад: {card.translation}")
+
+
+def main():
+    st.title("Додаток з мовними картками")
+
+    app = FlashcardApp()
+
+    menu = ["Додати картку", "Вивчати картки", "Переглянути всі картки", "Вибрати картки за категорією"]
+    choice = st.sidebar.selectbox("Виберіть опцію", menu)
+
+    if choice == "Додати картку":
+        app.add_flashcard()
+    elif choice == "Вивчати картки":
+        app.study_flashcards()
+    elif choice == "Переглянути всі картки":
+        app.view_all_flashcards()
+    elif choice == "Вибрати картки за категорією":
+        app.choose_category()
+
+
+if __name__ == "__main__":
+    main()
